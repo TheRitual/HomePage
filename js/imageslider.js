7874c12e4f86a4b05ac0cee0479b7ec91ebcca5d
Ritual <dev@theritual.eu>
2021-06-05 16:56:55 +0200

    added sliding menu for devices under 1024px

--- js/imageslider.js
+++ js/imageslider.js
@@ -1,7 +1,11 @@
 {
-    const linkBiogram = document.querySelector(".js-linkBiogram");
     const linkAboutMe = document.querySelector(".js-linkAboutMe");
     const linkContact = document.querySelector(".js-linkContact");
+    const linkBiogram = document.querySelector(".js-linkBiogram");
+    const linkCode = document.querySelector(".js-linkCode");
+    const linkExperience = document.querySelector(".js-linkExperience");
+    const linkGitHub = document.querySelector(".js-linkGitHub");
+
     const links = document.querySelectorAll(".js-menuLink");
 
     const topImageCover = document.querySelector(".js-topImageCover");
@@ -25,8 +29,12 @@
         useCover = !useCover;
     }
 
-    linkBiogram.addEventListener("click", () => { changeTop("dummy3.jpg", "Biogram"); });
+    
     linkAboutMe.addEventListener("click", () => { changeTop("monk.jpg", "About Me"); });
-    linkContact.addEventListener("click", () => { changeTop("dummy4.jpg", "Contact"); });
+    linkContact.addEventListener("click", () => { changeTop("monk.jpg", "Contact"); });
+    linkBiogram.addEventListener("click", () => { changeTop("monk.jpg", "Biogram"); });
+    linkCode.addEventListener("click", () => { changeTop("monk.jpg", "Coding"); });
+    linkExperience.addEventListener("click", () => { changeTop("monk.jpg", "Experience"); });
+    linkGitHub.addEventListener("click", () => { changeTop("monk.jpg", "Coding"); });
 
 }
--- js/menu.js
+++ js/menu.js
@@ -1,77 +1,101 @@
 {
-    document.querySelector(".js-menuAboutMe").addEventListener("mouseenter", () => {
+    let isMenuHidden = true;
+    const arrow = document.querySelector(".js-arrow");
+    const menu = document.querySelector(".js-menu");
+    const menuItems = document.querySelectorAll(".js-menuItem");
+    const menuAboutMe = document.querySelector(".js-menuAboutMe");
+
+
+    menuAboutMe.addEventListener("mouseenter", () => {
         document.querySelector(".js-menuAboutMeList").classList.remove("menu__subList--hidden");
-    })
+    });
 
-    document.querySelector(".js-menuAboutMe").addEventListener("mouseleave", () => {
+    menuAboutMe.addEventListener("mouseleave", () => {
         document.querySelector(".js-menuAboutMeList").classList.add("menu__subList--hidden");
-    })
+    });
 
-    document.querySelector(".js-menuAboutMe").addEventListener("focusin", () => {
+    menuAboutMe.addEventListener("focusin", () => {
         document.querySelectorAll(".js-menuSubList").forEach(element => {
             element.classList.add("menu__subList--hidden");
         });
         document.querySelector(".js-menuAboutMeList").classList.remove("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuCode").addEventListener("mouseenter", () => {
         document.querySelector(".js-menuCodeList").classList.remove("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuCode").addEventListener("mouseleave", () => {
         document.querySelector(".js-menuCodeList").classList.add("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuCode").addEventListener("focusin", () => {
         document.querySelectorAll(".js-menuSubList").forEach(element => {
             element.classList.add("menu__subList--hidden");
         });
         document.querySelector(".js-menuCodeList").classList.remove("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuFreeTime").addEventListener("mouseenter", () => {
         document.querySelector(".js-menuFreeTimeList").classList.remove("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuFreeTime").addEventListener("mouseleave", () => {
         document.querySelector(".js-menuFreeTimeList").classList.add("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuFreeTime").addEventListener("focusin", () => {
         document.querySelectorAll(".js-menuSubList").forEach(element => {
             element.classList.add("menu__subList--hidden");
         });
         document.querySelector(".js-menuFreeTimeList").classList.remove("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuProjects").addEventListener("mouseenter", () => {
         document.querySelector(".js-menuProjectsList").classList.remove("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuProjects").addEventListener("mouseleave", () => {
         document.querySelector(".js-menuProjectsList").classList.add("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuProjects").addEventListener("focusin", () => {
         document.querySelectorAll(".js-menuSubList").forEach(element => {
             element.classList.add("menu__subList--hidden");
         });
         document.querySelector(".js-menuProjectsList").classList.remove("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuPhilosophy").addEventListener("mouseenter", () => {
         document.querySelector(".js-menuPhilosophyList").classList.remove("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuPhilosophy").addEventListener("mouseleave", () => {
         document.querySelector(".js-menuPhilosophyList").classList.add("menu__subList--hidden");
-    })
+    });
 
     document.querySelector(".js-menuPhilosophy").addEventListener("focusin", () => {
         document.querySelectorAll(".js-menuSubList").forEach(element => {
             element.classList.add("menu__subList--hidden");
         });
         document.querySelector(".js-menuPhilosophyList").classList.remove("menu__subList--hidden");
-    })
+    });
 
+
+    arrow.addEventListener("click", () => {
+        if (isMenuHidden) {
+            arrow.innerHTML = "▲";
+            menu.classList.add("menu--displayed");
+        } else {
+            arrow.innerHTML = "▼";
+            menu.classList.remove("menu--displayed");
+        }
+        isMenuHidden = !isMenuHidden;
+    });
+
+    menuItems.forEach(element => {
+        element.addEventListener("click", (event) => {
+            event.target.classList.add("menu__item--active");
+        });
+    });
 }
